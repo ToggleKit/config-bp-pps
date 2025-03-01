0b68373295324d7f7b9c7d9f1238e4c962aee8cd
Toggle Kit <165154534+ToggleKit@users.noreply.github.com>
2025-03-01 18:38:45 +0600

    Update main.js

--- script/every/main.js
+++ script/every/main.js
@@ -154,7 +154,6 @@ document.querySelectorAll(".copyBTN").forEach(btn=>{
 btn.addEventListener("click", () => {
   const text = btn.closest("pre").querySelector("code").textContent;
   navigator.clipboard.writeText(text);
-  console.log(text)
 })
 })
 
