74e21cfc72e9659c35135b9720abb1b2d3b629e1
Toggle Kit <165154534+ToggleKit@users.noreply.github.com>
2025-03-02 12:33:56 +0600

    Update main.js

--- script/constant/main.js
+++ script/constant/main.js
@@ -1,14 +1,15 @@
-
 const sideBarBtn = document.querySelector("div#side-bar-toggle");
-const aside = document.querySelector("aside");
+const aside = document.querySelector("#HTML2");
+
 sideBarBtn.addEventListener("click", () => {
-  aside.getAttribute('id') ? aside.removeAttribute("id") : aside.setAttribute("id", "show")
-})
+  aside.classList.toggle("show");
+});
+
 const asideLink = aside.querySelectorAll('a');
 asideLink.forEach(a => {
   a.addEventListener('click', () => {
-    if (aside.id === 'show') {
-      aside.id = ""
+    if (aside.classList.contains('show')) {
+      aside.classList.remove("show");
     }
-  })
-})
+  });
+});
